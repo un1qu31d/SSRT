3b0f50169224b960b5c94f3dd0239e95687a8fa4
Muhammad Ali <un1qu31d@gmail.com>
2020-11-07 09:42:33 +0200

    add home page

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import Home from './pages/home';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
